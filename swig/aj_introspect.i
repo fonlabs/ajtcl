alljoyn/core-ajtcl

Adding SWIG wrapping files
Change-Id: Id74da2215694f1d0da5824978217e385bb2eb941
Signed-off-by: Javier Domingo Cansino <javier.domingo@fon.com>

swig/aj_introspect.i
@@ -1,0 +1,10 @@
+%include aj_target.i
+%include aj_status.i
+%include aj_bus.i
+%include aj_msg.i
+
+%{
+#include "aj_version.h"
+%}
+
+%include aj_version.h

fonlabs/ajtcl

swig/aj_introspect.i
@@ -1,0 +1,10 @@
+%include aj_target.i
+%include aj_status.i
+%include aj_bus.i
+%include aj_msg.i
+
+%{
+#include "aj_introspect.h"
+%}
+
+%include aj_introspect.h